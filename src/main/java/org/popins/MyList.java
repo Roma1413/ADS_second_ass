ff0beb48b1099fae353343ee28ae72ca371a582e
Ramazan Arinov <arinovramazan0101@gmail.com>
2025-07-20 19:49:31 +0500

    add interface

---
+++ src/main/java/org/popins/MyList.java
@@ -0,0 +1,11 @@
+package org.popins;
+
+public interface MyList<E> extends Iterable<E> {
+    void add(E element);
+    E get(int index);
+    void set(int index, E element);
+    E remove(int index);
+    int size();
+    boolean isEmpty();
+    void clear();
+}
